5408623800b911423453664ec906c8ae73d17597
mikechess <mikecook450@gmail.com>
2025-08-06 11:50:24 -0400

    Building a Chat loop

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 print("Hi, I am Albert, how can I help you today?")
-user_input = input("You: ")
-print(f"Cool, thanks for sharing that {user_input}")
+
+while True:
+    user_input = input("You: ")
+    if user_input == "exit":
+        break
+    print(f"Cool, thanks for sharing that {user_input}")
 
 
 

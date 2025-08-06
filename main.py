d5116bbfef769d164512a430457c31906daaaa62
mikechess <mikecook450@gmail.com>
2025-08-06 14:20:49 -0400

    Loading the AI API key and setup

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+gemini_key = os.getenv("GEMINI_API_KEY")
+
+print(gemini_key)
+
 print("Hi, I am Albert, how can I help you today?")
 
 while True:
@@ -6,5 +15,5 @@ while True:
         break
     print(f"Cool, thanks for sharing that {user_input}")
 
-
-
+response = "Hi there! How can I help you today? 😃"
+print(response)
